43a4bfa9e5d0d443d0cd5063b1973733816ae53c
daithi <webeire@gmail.com>
2018-05-30 02:40:24 +0100

    test getting results

--- test/lib/Cli_test.js
+++ test/lib/Cli_test.js
@@ -1,11 +1,46 @@
-describe.skip('CLI', function(){
+const assert = require('assert'),
+  Promises = require('bluebird'),
+  rewire = require('rewire');
 
-  it('will prompt github', function(done){
+let underTest;
 
+describe('CLI', function(){
+
+  beforeEach(function(){
+    underTest = rewire('../../lib/Cli');
   });
 
-  it('will search tweets', function(done){
+  it('will prompt github', function(){
 
+    const expected = require('./fixtures/ConsumerGithub_return.json'),
+      revert = underTest.__set__('github', {
+        searchRepos: function(){
+          return Promises.resolve(expected);
+        }
+      });
+
+    return underTest.promptGithub('foobar')
+      .then(actual => {
+        assert.deepEqual(actual, expected);
+        revert();
+      });
+  });
+
+  it('will search tweets', function(){
+
+    const expected = require('./fixtures/CliTweets_return.json'),
+      repos = require('./fixtures/ConsumerGithub_return.json'),
+      revert = underTest.__set__('twitter', {
+        searchTweets: function(){
+          return Promises.resolve(require('./fixtures/ConsumerTwitter_return.json'));
+        }
+      });
+
+    return underTest.searchTweets(repos)
+      .then(actual => {
+        assert.deepEqual(actual, expected);
+        revert();
+      });
   });
 
   it('will displayResults', function(done){
